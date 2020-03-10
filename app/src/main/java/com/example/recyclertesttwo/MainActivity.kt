c1d2573178c95febb5671e8f8de84e1862148af4
YeMinThu21200 <ymt.yeminthu02@gmail.com>
2020-03-10 10:27:49 +0630

    First Commit

---
+++ app/src/main/java/com/example/recyclertesttwo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.recyclertesttwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclertesttwo.adapter.FoodAdapter
+import com.example.recyclertesttwo.model.Food
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var foodArray = ArrayList<Food>()
+        foodArray.add(Food(R.drawable.sugar,"Fine Grain Sugar","Price-$20.00","1kg"))
+        foodArray.add(Food(R.drawable.peanut,"Peanuts","Price-$20.00","3kg"))
+        foodArray.add(Food(R.drawable.rice,"Dawat Rice","Price-$80.00","2kg"))
+
+        var foodAdapter = FoodAdapter(foodArray)
+        recyclerView.layoutManager= LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        recyclerView.layoutManager= GridLayoutManager(this,1)
+        recyclerView.adapter=foodAdapter
+    }
+}
---
+++ app/src/main/java/com/example/recyclertesttwo/model/food.kt
@@ -0,0 +1,8 @@
+package com.example.recyclertesttwo.model
+
+data class Food(
+   var imgfood:Int,
+   var food:String,
+   var price:String,
+   var weight:String
+)
